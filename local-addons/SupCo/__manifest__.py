c0082cb5d63ee79f00094d3a014fad586d86abd9
svseas <txt2408@gmail.com>
2023-10-06 19:34:21 +0700

    add qweb

---
+++ local-addons/SupCo/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'SupCo',
+    'version': '1.0',
+    'category': 'Custom',
+    'summary': 'Supreme Court Introduction Letter',
+    'depends': ['base'],
+    'data': [
+        'views/supco_views.xml',
+        'views/supco_menus.xml',
+        'reports/supco_letter.xml',
+        'reports/supco_letter_action.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ local-addons/SupCo/models/introduction_letter.py
@@ -0,0 +1,14 @@
+from odoo import api, fields, models
+
+
+class SupremeCourtLetter(models.Model):
+    _name = 'supreme.court.letter'
+    _description = 'Supreme Court Letter'
+
+    recipient_name = fields.Char(string='Recipient Name')
+    title_position = fields.Char(string='Title/Position')
+    organization_unit = fields.Char(string='Organization/Unit')
+    address = fields.Char(string='Address')
+    regarding = fields.Text(string='Regarding')
+    request = fields.Text(string='Request')
+    validity_date = fields.Date(string='Validity Date')
