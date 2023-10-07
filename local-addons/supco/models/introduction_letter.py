e1046b3cfbc3d72552f83f9b88438c89f019eea0
Truong Tang <txt2408@gmail.com>
2023-10-07 18:20:37 +0700

    Change field names to Vietnamese

--- local-addons/supco/models/introduction_letter.py
+++ local-addons/supco/models/introduction_letter.py
@@ -5,11 +5,12 @@ class SupremeCourtLetter(models.Model):
     _name = 'supreme.court.letter'
     _description = 'Supreme Court Letter'
 
-    number = fields.Char(string='Number')
-    recipient_name = fields.Char(string='Recipient Name')
-    title_position = fields.Char(string='Title/Position')
-    organization_unit = fields.Char(string='Organization/Unit')
-    address = fields.Char(string='Address')
-    regarding = fields.Text(string='Regarding')
-    request = fields.Text(string='Request')
-    validity_date = fields.Date(string='Validity Date')
+    number = fields.Integer(string='Số')
+    recipient_name = fields.Char(string='Đồng chí')
+    title_position = fields.Char(string='Chức vụ')
+    organization_unit = fields.Char(string='Đơn vị')
+    address = fields.Char(string='Cử đến')
+    regarding = fields.Text(string='Về việc')
+    request = fields.Text(string='Yêu cầu (Tên các đồng chí)')
+    validity_date = fields.Date(string='Ngày hiệu lực')
+    created_by = fields.Many2one('res.users', string='Tạo bởi', default=lambda self: self.env.user)
