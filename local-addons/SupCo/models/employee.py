072c773330ec7446fb3b5939a8a4e8200d96466b
tranviet16 <tranviet16@gmail.com>
2023-10-08 17:29:54 +0700

    Add Models

---
+++ local-addons/SupCo/models/employee.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class Employee(models.Model):
+    _name = "supreme.court.employee"
+    _description = 'Supreme Court Employee'
+
+    name = fields.Char(string="Full Name")
+    title_position = fields.Char(string="Title Position")
+    dob = fields.Date(string='Date of Birth')
+    national_id = fields.Char(string='National ID')
+    department = fields.Many2many('supreme.court.department',string='Department')
+    introduction_letter = fields.Many2many("supreme.court.letter",string="Supreme Court Letter")
