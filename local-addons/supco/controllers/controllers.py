ec21b6e58559adb688b84e1756f4790856206599
Truong Tang <txt2408@gmail.com>
2023-10-15 23:52:59 +0700

    duplicate

--- local-addons/supco/controllers/controllers.py
+++ local-addons/supco/controllers/controllers.py
@@ -45,42 +45,68 @@ class UserController(http.Controller):
             return "User not found or You have no right to access."
 
 
-class LetterController(http.Controller):
+class ReportDuplicator(http.Controller):
+    @http.route('/letters/<int:id>', type='http', auth='public')
+    def duplicate_report_and_qr(self, id, **kwargs):
+        # Retrieve the original report record
+        original_report = request.env['supco.report_supreme_court_letter_main'].browse(id)
 
-    @http.route('/letters/qr/<int:letter_id>', type='http', auth="public", website=True)
-    def letter_qr(self, letter_id):
-        # Fetch the letter from the database
-        letter = request.env['supreme.court.letter'].sudo().browse(letter_id)
-        if not letter:
-            return "Letter not found!"
+        if not original_report:
+            return "Original report not found!"
+
+        # Duplicate the content of the report by creating a new record
+        duplicated_report = original_report.copy()
+
+        # Render the duplicated report as base64-encoded PDF
+        base64_encoded_report = self._render_report(duplicated_report)
 
         # Generate a unique identifier (token) for the public access
         token = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
 
-        # Generate QR code from the token
+        # Generate a URL containing the token
+        qr_code_link = f'/my_module/duplicate_report/{duplicated_report.id}/{token}'
+
+        # Generate a QR code from the token
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=10,
             border=4,
         )
-        qr.add_data(token)
+        qr.add_data(qr_code_link)
         qr.make(fit=True)
-        img = qr.make_image(fill_color="black", back_color="white")
+        qr_img = qr.make_image(fill_color="black", back_color="white")
 
-        # Save the QR code as a public file with a unique filename
-        public_filename = f"public_letter_{letter_id}_{token}.png"
-        img.save(public_filename)
+        # Save the QR code as a base64-encoded image
+        buffer = io.BytesIO()
+        qr_img.save(buffer, format="PNG")
+        base64_encoded_qr = base64.b64encode(buffer.getvalue()).decode()
 
-        # Read the QR code image
-        with open(public_filename, 'rb') as qr_file:
-            qr_code_data = qr_file.read()
+        # Update the duplicated report's QR code field with the base64-encoded image
+        duplicated_report.write({'qr_code': base64_encoded_qr})
 
-        # Update response headers to force a download prompt
-        headers = [
-            ('Content-Type', 'image/png'),
-            ('Content-Disposition', content_disposition(public_filename))
-        ]
+        # Generate the PDF report using the provided snippet
+        pdf, _ = self._render_qweb_pdf('sale.action_report_saleorder', [duplicated_report.id])
 
-        # Return the QR code image as a response
-        return http.request.make_response(qr_code_data, headers=headers)
+        # Return the base64-encoded report as a response for downloading
+        pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', u'%s' % len(pdf))]
+        response = request.make_response(pdf, headers=pdfhttpheaders)
+        response.headers['Content-Disposition'] = f'attachment; filename=duplicated_report.pdf'
+        return response
+
+    def _render_report(self, report_record):
+        # Retrieve the report template associated with the model
+        report_template = report_record.get_report_template()
+
+        # Generate the report content using Odoo's report generation mechanism
+        report_service = report_template.report_name
+        report_data, format = report_template.render_qweb_pdf([report_record.id])
+
+        # Encode the report content as base64
+        base64_encoded_report = base64.b64encode(report_data).decode()
+
+        return base64_encoded_report
+
+    def _render_qweb_pdf(self, report_name, docargs):
+        pdf, _ = request.env['ir.actions.report'].sudo()._render_qweb_pdf(report_name, docargs)
+        return pdf
