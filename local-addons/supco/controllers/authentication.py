9b7ca0e0405885557469d84f6da65679d6def8d9
Truong Tang <txt2408@gmail.com>
2024-01-02 23:50:28 +0700

    fix jitsi settings

--- local-addons/supco/controllers/authentication.py
+++ local-addons/supco/controllers/authentication.py
@@ -3,6 +3,7 @@ from odoo.http import request
 import jwt
 from werkzeug.utils import redirect
 import logging
+import time
 
 _logger = logging.getLogger(__name__)
 
@@ -14,16 +15,38 @@ class Auth(http.Controller):
         params = request.params
         login = params.get('login')
         password = params.get('password')
+        
 
-        uid = request.session.authenticate('postgres', login, password)
+        # _logger.info(list(request.env.registry.models.keys()))
+        _logger.info(f"Login: {login}")
+        _logger.info(f"Password: {password}")
+
+        uid = request.session.authenticate('odoo', login, password)
+
         _logger.info(f"User {login} with {uid} authenticated successfully")
         if uid is not False:
             user = request.env['res.users'].sudo().search([('id', '=', uid)])
-            token = jwt.encode({'user_id': user.id}, '123456789', algorithm='HS256')
+            payload = {
+                'user_id': user.id,
+                'nbf': int(time.time()),
+                'exp': int(time.time()) + 3600,  
+                'context': {
+                    "user": {
+                        "name": user.name,
+                        "email": user.email,
+                        "avatar": user.image_1920,  
+                    }
+                },
+                "aud":"jitsi",
+                "iss":"jitsi",
+                "sub":"*",
+                "room": roomName,
+            }
+            token = jwt.encode(payload, '123456789', algorithm='HS256')
             print(f"User {user.name} authenticated successfully")
-            return http.redirect(f"https://suncat.io/{roomName}?jwt={token}")
+            return {"redirect": f"https://suncat.io/{roomName}?jwt={token}"}
         else:
-            return http.redirect(f"https://suncat.io/{roomName}?auth_status=unauthorized")
+            return {"redirect": f"https://suncat.io/{roomName}?auth_status=unauthorized"}
 
 class Redirect(http.Controller):
     @http.route('/api/redirect', type='http', auth='public', methods=['GET'])
