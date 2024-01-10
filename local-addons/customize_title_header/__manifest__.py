7bc6c342ae299073a4a4c678705633ea846ad5ce
hglong16 <intihad.vuong@gmail.com>
2024-01-10 13:25:06 +0700

    Add company favicon and title customization

--- local-addons/customize_title_header/__manifest__.py
+++ local-addons/customize_title_header/__manifest__.py
@@ -6,11 +6,6 @@
     "version": "15.0.1.0.0",
     "sequence": 1,
     "summary": """
-        Custom shortcut title, Odoo Favicon Title, Odoo Title and Favicon, Odoo Backend Title Favicon, Odoo Web Favicon Title, 
-        Odoo Web Title, Odoo Customize Title Header, Odoo Browser Header, Odoo Header Favicon, Odoo Header Title, Web Window Title, 
-        Web Backend Title, Odoo Backend Title, Web Responsive Title, Remove Odoo Favicon Header, Remove Odoo Title, Hide Odoo Title,
-        Remove Favicon Header, Web Odoo Shortcut Favicon Shortcut Odoo Shortcut, Odoo Backend Favicon Odoo Backend Title Odoo Browser,
-        Customization Favicon Title Customization, Configutable Favicon Title Configutable, Configution Favicon Title Configution, 
         Web Shortcut Customization Shortcut Editable Favicon Editable Shortcut Favicon Setup Title Header Title Browser Title Navigator
     """,
     "description": "Choose your own Title to display on the browser header.",
@@ -22,7 +17,7 @@
     "license": "LGPL-3",
     "images": ["static/description/wallpaper.png"],
     "depends": ["web"],
-    "data": ["views/title.xml"],
+    "data": ["views/title.xml", "views/company.xml"],
     "assets": {
         "web.assets_backend_prod_only": [
             "customize_title_header/static/src/js/favicon.js",
