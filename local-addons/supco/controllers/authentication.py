53fd43d9fb825b8e3eec7de85cc1d950ab1d83f9
svseas <txt2408@gmail.com>
2024-03-01 12:32:53 +0700

    add room to route

--- local-addons/supco/controllers/authentication.py
+++ local-addons/supco/controllers/authentication.py
@@ -9,7 +9,8 @@ import json
 _logger = logging.getLogger(__name__)
 
 # class Auth(http.Controller):
-#     @http.route('/api/auth', type='json', auth='public', methods=['POST'], csrf=False)
+#     @http.route('/api/auth', type='json', auth='public', methods=['POST'], csrf=False, cors="*")
+
 #     def authenticate(self, **kwargs):
 #         params = request.params
 #         login = params.get('login')
@@ -26,7 +27,7 @@ _logger = logging.getLogger(__name__)
 #             payload = {
 #                 'context': {
 #                     "user": {
-#                         "id": user.login,  
+#                         "id": user.login,
 #                         "name": user.name,
 #                         "email": user.email,
 #                     }
@@ -53,33 +54,49 @@ _logger = logging.getLogger(__name__)
 #                 "status": 403
 #             }
 
+
 class Auth(http.Controller):
-    @http.route('/api/auth', type='json', auth='public', methods=['POST', 'OPTIONS'])
+    @http.route(
+        "/api/auth",
+        type="json",
+        auth="public",
+        methods=["POST", "OPTIONS"],
+        csrf=False,
+        cors="*",
+    )
     def authenticate(self, **kwargs):
         headers = [
-            ('Access-Control-Allow-Origin', '*'),
-            ('Access-Control-Allow-Methods', 'POST, OPTIONS'),
-            ('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'),
+            ("access-control-allow-origin", "*"),
+            ("access-control-allow-methods", "POST, OPTIONS"),
+            (
+                "access-control-allow-headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+            ),
         ]
-        
-        if request.httprequest.method == 'OPTIONS':
-            return request.make_response('', headers=headers)
+
+        if (
+            request.httprequest.method == "OPTIONS"
+            or request.httprequest.method == "GET"
+            or request.httprequest.method == "HEAD"
+            or request.httprequest.method == "TRACE"
+        ):
+            return request.make_response("", headers=headers)
 
         params = request.params
-        login = params.get('login')
-        password = params.get('password')
+        login = params.get("login")
+        password = params.get("password")
 
         _logger.info(f"Login attempt for: {login}")
 
-        uid = request.session.authenticate('odoo', login, password)
+        uid = request.session.authenticate("odoo", login, password)
 
         if uid is not False:
-            user = request.env['res.users'].sudo().search([('id', '=', uid)])
+            user = request.env["res.users"].sudo().search([("id", "=", uid)])
             current_time = int(time.time())
             payload = {
-                'context': {
+                "context": {
                     "user": {
-                        "id": user.login,  
+                        "id": user.login,
                         "name": user.name,
                         "email": user.email,
                     }
@@ -88,20 +105,26 @@ class Auth(http.Controller):
                 "iss": "jitsi",
                 "nbf": current_time,
                 "exp": current_time + 3600,
-                "iat": current_time
+                "iat": current_time,
+                "room": "*",
             }
-            jwt_secret = 'YourSecretKeyHere'
-            token = jwt.encode(claims=payload, key=jwt_secret, algorithm='HS256')
+            jwt_secret = "YourSecretKeyHere"
+            token = jwt.encode(claims=payload, key=jwt_secret, algorithm="HS256")
             _logger.info(f"User {user.name} authenticated successfully")
             _logger.info(f"Token: {token}")
-            
-            return {
-                "jwt": token
-            }
+
+            response = request.make_response(json.dumps({"jwt": token}))
+            response.headers["content-type"] = "application/json"
+            response.headers["access-control-allow-origin"] = "*"  # CORS
+            response.headers["access-control-allow-methods"] = "POST, OPTIONS"
+            response.headers[
+                "access-control-allow-headers"
+            ] = "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+
+            return {"jwt": token}
         else:
             return {
                 "error": "Unauthorized access",
                 "message": "Invalid login or password",
-                "status": 403
+                "status": 403,
             }
-
